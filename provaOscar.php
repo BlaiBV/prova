301b384e9d12b21228295a989cf3ad4b0b80d625
Oscar-Vidal-Serrano <ovidal@inscaparrella.cat>
2023-11-14 19:14:10 +0100

    Update provaOscar.php

--- provaOscar.php
+++ provaOscar.php
@@ -7,7 +7,11 @@
 </head>
 <body>
     <h1>Hola Món</h1>
+<<<<<<< Updated upstream
     <p>COMMIT BLAI</p>
     <h3>ALGO</h3>
+=======
+    <h1>Hola Món</h1>
+>>>>>>> Stashed changes
 </body>
 </html>
